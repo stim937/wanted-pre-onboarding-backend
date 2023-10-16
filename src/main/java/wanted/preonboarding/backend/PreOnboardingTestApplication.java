d6bf46ea346c82bb853df19e70d0638851d7429a
LEE JI UI <stim937@naver.com>
2023-10-16 15:41:47 +0900

    프로젝트 생성

---
+++ src/main/java/wanted/preonboarding/backend/PreOnboardingTestApplication.java
@@ -0,0 +1,13 @@
+package wanted.preonboarding.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnboardingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreOnboardingTestApplication.class, args);
+	}
+
+}
